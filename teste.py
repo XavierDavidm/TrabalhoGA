e6d01333d742dfc27a6ebd1701a4b8c17be09ac0
XavierDavidm <davidmoreirax15@hotmail.com>
2024-09-24 19:59:17 -0300

    Trabalho grau A inicio

---
+++ teste.py
@@ -0,0 +1,26 @@
+#teste mesmo pais
+pais1=pais(('chile','uruguai','argentina'))
+pais1.registarIso()
+pais2=pais()
+pais2.registarIso()
+pais1.VerificarIgual(pais2)
+
+#teste vizinho
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
